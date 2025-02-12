dd6e8ba2f41201bfe33c5ed97dfae8f549e379c8
LuisDavid <luisdavid25torrealba@gmail.com>
2025-02-12 12:28:21 -0400

    Creacion del repositorio

---
+++ src/components/Confirma.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+//Estilos diseño
+import './Home.css';
+import './CuentaRegresiva.css';
+
+const Confirma = () => {
+  return (
+    <>
+
+      
+        <div className='Confirma-container'>
+          
+                  <div className='Confirma-text'>
+                      <a href='https://walink.co/8423e0' style={{textDecoration:'none'}}>
+                        <button className='btn-outline-one'
+                            >Confirma Aquí
+                        </button>
+                      </a>
+                  </div>
+
+                  <div className='Confirma-text-two'>
+                      <a href='https://drive.google.com/file/d/1YK9Ii2fVvYnOPCQ4-rhVT9Lh5cIwJlb9/view' style={{textDecoration:'none'}}>
+                        <button className='btn-outline-one'
+                            >Click Aquí
+                        </button>
+                      </a>
+                  </div>
+          
+          </div>  
+    </>
+  )
+}
+
+export default Confirma;
---
+++ src/components/EndLogo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Estilos diseño
+import './Home.css';
+
+const EndLogo = () => {
+  return (
+    <>
+        <div className='EndLogo-container'></div>  
+    </>
+  )
+}
+
+export default EndLogo;
